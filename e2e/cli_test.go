142a84a82836de8632d4c6f0a2942b623ef92941
Tim Hardeck <thardeck@suse.de>
2019-04-04 16:34:13 +0200

    Add e2e and integration tests
    Co-authored-by: Mario Manno <mario.manno@suse.com>

---
+++ e2e/cli_test.go
@@ -0,0 +1,93 @@
+package e2e_test
+
+import (
+	"os"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("CLI", func() {
+	act := func(arg ...string) (session *gexec.Session, err error) {
+		cmd := exec.Command(cliPath, arg...)
+		session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		return
+	}
+
+	Describe("help", func() {
+		It("should show the help for server", func() {
+			session, err := act("help")
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(session.Out).Should(Say(`Usage:`))
+		})
+
+		It("should show all available options for server", func() {
+			session, err := act("help")
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(session.Out).Should(Say(`Flags:
+  -o, --docker-image-org string          Dockerhub organization that provides the operator docker image \(default "cfcontainerization"\)
+  -r, --docker-image-repository string   Dockerhub repository that provides the operator docker image \(default "cf-operator"\)
+  -t, --docker-image-tag string          Tag of the operator docker image \(default "\d+.\d+.\d+"\)
+  -h, --help                             help for cf-operator
+  -c, --kubeconfig string                Path to a kubeconfig, not required in-cluster
+  -n, --namespace string                 Namespace to watch for BOSH deployments \(default "default"\)
+  -w, --operator-webhook-host string     Hostname/IP under which the webhook server can be reached from the cluster
+  -p, --operator-webhook-port string     Port the webhook server listens on \(default "2999"\)`))
+		})
+
+		It("shows all available commands", func() {
+			session, err := act("help")
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(session.Out).Should(Say(`Available Commands:
+  help                   Help about any command
+  version                Print the version number
+
+`))
+		})
+	})
+
+	Describe("default", func() {
+		It("should start the server", func() {
+			session, err := act()
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(session.Err).Should(Say(`Starting cf-operator \d+\.\d+\.\d+ with namespace`))
+		})
+
+		Context("when specifying namespace", func() {
+			Context("via environment variables", func() {
+				BeforeEach(func() {
+					os.Setenv("NAMESPACE", "env-test")
+				})
+
+				AfterEach(func() {
+					os.Setenv("NAMESPACE", "")
+				})
+
+				It("should start for namespace", func() {
+					session, err := act()
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session.Err).Should(Say(`Starting cf-operator \d+\.\d+\.\d+ with namespace env-test`))
+				})
+			})
+
+			Context("via using switches", func() {
+				It("should start for namespace", func() {
+					session, err := act("--namespace", "switch-test")
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(session.Err).Should(Say(`Starting cf-operator \d+\.\d+\.\d+ with namespace switch-test`))
+				})
+			})
+		})
+	})
+
+	Describe("version", func() {
+		It("should show a semantic version number", func() {
+			session, err := act("version")
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(session.Out).Should(Say(`CF-Operator Version: \d+.\d+.\d+`))
+		})
+	})
+})
